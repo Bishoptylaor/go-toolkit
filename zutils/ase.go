785b8216898b97de725d47374030d1f7920343d9
wangxinyu <19482929+Bishoptylaor@users.noreply.github.com>
2024-07-17 18:25:17 +0800

    [feat]zutils.ase fmt

--- zutils/ase.go
+++ zutils/ase.go
@@ -31,18 +31,18 @@ import (
 // 填充明文
 func _PKCS5Padding(plaintext []byte, blockSize int) []byte {
 	padding := blockSize - len(plaintext)%blockSize
-	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
-	return append(plaintext, padtext...)
+	padText := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(plaintext, padText...)
 }
 
 // 去除填充数据
 func _PKCS5UnPadding(origData []byte) []byte {
 	length := len(origData)
-	unpadding := int(origData[length-1])
-	return origData[:(length - unpadding)]
+	unPadding := int(origData[length-1])
+	return origData[:(length - unPadding)]
 }
 
-// AES加密
+// AesEncrypt 加密
 func AesEncrypt(origData, key []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
@@ -53,22 +53,22 @@ func AesEncrypt(origData, key []byte) ([]byte, error) {
 	blockSize := block.BlockSize()
 	origData = _PKCS5Padding(origData, blockSize)
 	blockMode := cipher.NewCBCEncrypter(block, key[:blockSize]) // 初始向量的长度必须等于块block的长度16字节
-	crypted := make([]byte, len(origData))
-	blockMode.CryptBlocks(crypted, origData)
-	return crypted, nil
+	encrypted := make([]byte, len(origData))
+	blockMode.CryptBlocks(encrypted, origData)
+	return encrypted, nil
 }
 
-// AES加密 结果返回base64编码后的string
+// AesEncryptWithString 加密 结果返回base64编码后的string
 func AesEncryptWithString(plainText string, key string) (string, error) {
-	crypted, err := AesEncrypt([]byte(plainText), []byte(key))
+	encrypted, err := AesEncrypt([]byte(plainText), []byte(key))
 	if err != nil {
 		return "", err
 	}
-	return base64.StdEncoding.EncodeToString(crypted), nil
+	return base64.StdEncoding.EncodeToString(encrypted), nil
 }
 
-// AES解密
-func AesDecrypt(crypted, key []byte) ([]byte, error) {
+// AesDecrypt 解密
+func AesDecrypt(encrypted, key []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
@@ -77,19 +77,19 @@ func AesDecrypt(crypted, key []byte) ([]byte, error) {
 	// AES分组长度为128位，所以blockSize=16，单位字节
 	blockSize := block.BlockSize()
 	blockMode := cipher.NewCBCDecrypter(block, key[:blockSize]) // 初始向量的长度必须等于块block的长度16字节
-	origData := make([]byte, len(crypted))
-	blockMode.CryptBlocks(origData, crypted)
+	origData := make([]byte, len(encrypted))
+	blockMode.CryptBlocks(origData, encrypted)
 	origData = _PKCS5UnPadding(origData)
 	return origData, nil
 }
 
-// AES解密, 输入的加密串需是已经用base64编码过的
+// AesDecryptWithString 解密, 输入的加密串需是已经用base64编码过的
 func AesDecryptWithString(secretText string, key string) (string, error) {
-	crypted, err := base64.StdEncoding.DecodeString(secretText)
+	encrypted, err := base64.StdEncoding.DecodeString(secretText)
 	if err != nil {
 		return "", err
 	}
-	res, err := AesDecrypt(crypted, []byte(key))
+	res, err := AesDecrypt(encrypted, []byte(key))
 	if err != nil {
 		return "", err
 	}
