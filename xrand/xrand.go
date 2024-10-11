ad3881c6f66e3826bdbfc2c81f0ca5387543fdcf
wangxinyu <19482929+Bishoptylaor@users.noreply.github.com>
2024-10-11 11:17:05 +0800

    [feat]xrand

--- xrand/xrand.go
+++ xrand/xrand.go
@@ -1,10 +1,3 @@
-package zrand
-
-import (
-	"math/rand"
-	"time"
-)
-
 /*
  *  ┏┓      ┏┓
  *┏━┛┻━━━━━━┛┻┓
@@ -23,8 +16,15 @@ import (
  *      ┗┻┛　 ┗┻┛
  @Time    : 2024/7/13 -- 14:53
  @Author  : bishop ❤️ MONEY
- @Description: zrand.go
+ @Description: xrand.go
 */
+
+package xrand
+
+import (
+	"math/rand"
+	"time"
+)
 
 var RanD random
 
@@ -38,9 +38,8 @@ const (
 	PROBABILITY int32 = 10000
 )
 
-func (r *random) InitProb() (err error) {
+func init() {
 	rd = rand.New(rand.NewSource(time.Now().UnixNano()))
-	return nil
 }
 
 func (r *random) GetRandomNumber(n int32) int32 {
@@ -73,4 +72,40 @@ func (r *random) calRandomNumberBetween(min, max int32) int32 {
 		return cal
 	}
 	return 1
+}
+
+func Wave(base int64, lowRate int64, upRate int64) int64 {
+	rate := rand.Intn(int((upRate-lowRate)+1)) + int(lowRate)
+
+	return base + int64(float64(base)*float64(rate)/100)
+}
+
+func RandBetween(start int64, end int64) int64 {
+	if start > end {
+		return 0
+	}
+	return int64(rand.Intn(int(end-start)+1)) + start
+}
+
+// choose n from m eg: we have m=10 and n=5, then we will get [1,3,5,7,8]
+func ChooseMN(m, n int) []int {
+	chosen := make([]int, 0, n)
+	if m <= n {
+		for i := 0; i < m; i++ {
+			chosen = append(chosen, i)
+		}
+		return chosen
+	}
+	choosed := map[int]bool{}
+	for {
+		if len(chosen) >= n {
+			break
+		}
+		idx := rand.Intn(m)
+		if !choosed[idx] {
+			chosen = append(chosen, idx)
+			choosed[idx] = true
+		}
+	}
+	return chosen
 }
